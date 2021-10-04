ec00db85adf3df04fe0b52880e80191f0a248fce
Natalia Gonzalez <nataliasita04@gmail.com>
2021-10-04 01:25:46 -0500

    ajuste tarjetas

--- memorygame/data.js
+++ memorygame/data.js
@@ -1,27 +1,27 @@
 const data = [
-    { "id": 1, "name": "agua", "url": "../memoryGame/assets/images1/agua.jpg"},
+    { "id": 1, "name": "agua", "url": "../memorygame/assets/images1/agua.jpg"},
   
-    { "id": 2, "name": "oxígeno", "url": "../memoryGame/assets/images1/oxigeno.jpg" },
+    { "id": 2, "name": "oxígeno", "url": "../memorygame/assets/images1/oxigeno.jpg" },
   
-    { "id": 3, "name": "azufre", "url": "../memoryGame/assets/images1/azufre.jpg" },
+    { "id": 3, "name": "azufre", "url": "../memorygame/assets/images1/azufre.jpg" },
   
-    { "id": 4, "name": "nitrógeno", "url": "../memoryGame/assets/images1/nitrogeno.jpg" },
+    { "id": 4, "name": "nitrógeno", "url": "../memorygame/assets/images1/nitrogeno.jpg" },
   
-    { "id": 5, "name": "amoníaco", "url": "../memoryGame/assets/images1/amoniaco.jpg" },
+    { "id": 5, "name": "amoníaco", "url": "../memorygame/assets/images1/amoniaco.jpg" },
   
-    { "id": 6, "name": "metano", "url": "../memoryGame/assets/images1/metano.jpg" },
+    { "id": 6, "name": "metano", "url": "../memorygame/assets/images1/metano.jpg" },
   
-    { "id": 7, "name": "ácido sulfhídrico", "url": "../memoryGame/assets/images1/acidosulfurico.jpg" },
+    { "id": 7, "name": "ácido sulfhídrico", "url": "../memorygame/assets/images1/acidosulfurico.jpg" },
   
-    { "id": 8, "name": "temperaturas extremas", "url": "../memoryGame/assets/images1/temperaturas.jpg" },
+    { "id": 8, "name": "temperaturas extremas", "url": "../memorygame/assets/images1/temperaturas.jpg" },
   
-    { "id": 9, "name": "hidrógeno molecular", "url": "../memoryGame/assets/images1/hidrogenoMolecular.jpg" },
+    { "id": 9, "name": "hidrógeno molecular", "url": "../memorygame/assets/images1/hidrogenoMolecular.jpg" },
   
-    { "id": 10, "name": "dióxido de carbono", "url": "../memoryGame/assets/images1/dioxidodecarbono.jpg" },
+    { "id": 10, "name": "dióxido de carbono", "url": "../memorygame/assets/images1/dioxidodecarbono.jpg" },
   
-    { "id": 11, "name": "Galileo", "url": "../memoryGame/assets/images1/galileo sonda.jpg" },
+    { "id": 11, "name": "Galileo", "url": "../memorygame/assets/images1/galileo sonda.jpg" },
   
-    { "id": 12, "name": "Cassini-Huygens", "url": "../memoryGame/assets/images1/cassini.jpg" }
+    { "id": 12, "name": "Cassini-Huygens", "url": "../memorygame/assets/images1/cassini.jpg" }
 ]
 
 export default data;
