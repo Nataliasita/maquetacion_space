998e0a3ccd99a050f7b59c30e9da8fcd92a12eea
Natalia Gonzalez <nataliasita04@gmail.com>
2021-10-05 20:13:57 -0500

    ajustes vista tarjetas

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const content = document.getElementById('root');
 const game = document.getElementById('game');
 const sidebar = document.getElementById('sidebar');
 
-import memorygame from '../memorygame/memorygame.js'
-import data from "../memorygame/data.js"
+import memorygame from './memorygame/memorygame.js'
+import data from "./memorygame/data.js"
 
 window.addEventListener('load', () => {
     // let startGame = false;
